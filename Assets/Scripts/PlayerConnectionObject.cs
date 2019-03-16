4cf9e9b0bcc9dd4dbdf9c1fda1a5b8be2c303dbb
Marco Cordoni <marco.cordoni@outlook.it>
2019-03-16 12:01:19 +0100

    Fist Commit
    Struttare di base di un network in Unity

---
+++ Assets/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; // importo la libreria per il network
+
+public class PlayerConnectionObject : NetworkBehaviour { // serve NetworkBehaviour
+
+    public GameObject PlayerUnitPrefab; // prefab del player da instanziare
+    [SyncVar(hook = "OnPlayerNameChanged")] public string playerName = "Anonymous"; // il tag SyncVar serve per fare in modo che se un valore cambia sul server tutti i client ne siano subito informati, l'hook invece serve per associare alla variabile una funzione che verrà richiamata ogni volta che la variabile viene modificata, passando alla funzione la variabile in questione
+
+    void Start() { // ATTENZIONE: Start viene eseguito su tutti i client, anche se non posseggono quel player
+        if (!isLocalPlayer) // se non è il mio local player esco e non faccio altro, ciò serve per evitare che start venga eseguito da tutti i client
+            return; 
+        CmdSpawnMyUnit(); // instanzia il player e richiama il metodo NetworkServer.Spawn() sul server perchè quest'ultimo PUO' ESSERE ESEGUITO SOLO DAL SERVER
+    }
+
+    void Update() { // ATTENZIONE: Update viene eseguito su tutti i client, anche se non posseggono quel player
+        if (!isLocalPlayer) // controllo che sia il mio update, se no esco
+            return;
+
+        if (Input.GetKeyDown(KeyCode.S)) // se il giocatore preme S creo un'altra istanza 
+            CmdSpawnMyUnit(); // crea il player e lo notifica a tutti i client
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            string n = "Quill " + Random.Range(1, 100);
+            Debug.Log("Send request = old name: " + playerName + " ;new name: " + n);
+            CmdChangePlayerName(n);
+        }
+    }
+
+    void OnPlayerNameChanged(string newName) {
+        Debug.Log("OnPlayerNameChanged = Old Name: " + playerName + "; New name: " + newName);
+        playerName = newName; // ATTENZIONE: se si usa un hook su una SyncVar, il nostro valore locale non verrà aggiornato automaticamente, è necessario dirglielo esplicitamente anche qui
+        gameObject.name = "PlayerConnectionObject [" + newName + "]";
+    }
+
+    
+    /* ---------------------- COMMAND ESEGUITI DAL CLIENT SUL SERVER, d'ora in poi sono sul server ---------------------- */
+
+    [Command] // indico che questa funzione anche se viene richiamata da tutti i client dovrà essere eseguita solo dal server
+    void CmdSpawnMyUnit() { // per convenzione OBBLIGATORIA questi metodi eseguiti dal server devono iniziare con "Cmd"
+        GameObject go = Instantiate(PlayerUnitPrefab); // instanzio il player, che però è solo in locale, gli altri client ancora non sanno che esiste
+
+        // PlayerUnitPrefab DEVE ESSERE AGGIUNTO ALLA LISTA DEI PREFABS SPAWNABILI NEL NETWORK MANAGER, ALTRIMENTI AVREMO UN ERRORE
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient); // ora attraverso SpawnWithClientAuthority tutti i client conoscono questo player e gli assegno anche l'autorità (hasAuthority = true)
+    }
+
+    [Command]
+    void CmdChangePlayerName(string n) {
+        Debug.Log("CmdChangePlayerName: " + n);
+        playerName = n;
+
+        // qui posso verificare che il nome non contenga parole proibite
+
+        //RpcChangePlayerName(playerName); // SE USO SyncVar NON SERVE CHE NOTIFICO MANUALMENTE IL CAMBIAMENTO AI CLIENT
+    }
+
+
+    /* ---------------------- RPC ESEGUITI DAL SERVER SUI CLIENT, d'ora in poi sono sul client ---------------------- */
+
+    /*[ClientRpc] // tag che indica i comandi eseguiti solo dai client
+    void RpcChangePlayerName(string n) { // devono per forza iniziare con Rpc, SE USO SyncVar NON SERVE CHE NOTIFICO MANUALMENTE IL CAMBIAMENTO AI CLIENT
+        Debug.Log("RpcChangePlayerName" + n);
+        playerName = n;
+    }*/
+}
---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerUnit : NetworkBehaviour {
+
+    Vector3 velocity;
+
+    // The position we think is most correct for this player. NOTE: if we are the authority, then this will be exactly the same as transform.position
+    Vector3 bestGuessPosition;
+
+    // This is a constantly updated value about our latency to the server (i.e. how many second it takes for us to receive a one-way message)
+    float ourLatency; // TODO: This should probably be something we get from the PlayerConnectionObject
+
+    // This higher this value, the faster our local position will match the best guess position
+    float latencySmoothingFactor = 10;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.Translate(velocity * Time.deltaTime); // questa parte di codice è eseguita da tutte le versione di questo object in esecuzione, anche se non ne hanno l'autorità
+
+        if (!hasAuthority) { // true se è un oggetto che possiedo sul mio pc e ho l'autorità per usarlo direttamente, bisogna però assegnare questa autorità al client
+
+            // We aren't the authority for this object, but we still need to update our local position for this object based on our best guess of where it probably is on the owning player's screen.
+            bestGuessPosition = bestGuessPosition + (velocity * Time.deltaTime);
+
+            // Instead of TELEPORTING our position to the best guess's position, we can smoothly lerp to it.
+            transform.position = Vector3.Lerp(transform.position, bestGuessPosition, Time.deltaTime * latencySmoothingFactor);
+
+            return;
+        }
+
+        // If we get to here, we are the authoritative owner of this object
+        transform.Translate(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space)) // se premo space l'oggetto si alza verso l'alto, spetterà poi al network trasform di notificarlo al server e poi ai client
+            this.transform.Translate(0, 1, 0);
+        
+        if (Input.GetKeyDown(KeyCode.D)) // se premo D distruggo l'object
+            Destroy(gameObject);
+
+        if ( /* some input */ true) {
+            // The player is asking us to change our direction/speed (i.e. velocity)
+            velocity = new Vector3(1, 0, 0);
+            CmdUpdateVelocity(velocity, transform.position);
+        }
+    }
+
+
+    /* ---------------------- COMMAND ESEGUITI DAL CLIENT SUL SERVER ---------------------- */
+    
+    [Command]
+    void CmdUpdateVelocity(Vector3 v, Vector3 p) {
+
+        //Sul server
+        velocity = v;
+        transform.position = p;
+
+        // If we know what our current latency is, we could do something like this:
+        //  transform.position = p + (v * (thisPlayersLatencyToServer))
+
+        //Notifico ai client la posizione
+        RpcUpdateVelocity(velocity, transform.position);
+    }
+
+
+    /* ---------------------- RPC ESEGUITI DAL SERVER SUI CLIENT -------------------------- */
+
+    [ClientRpc]
+    void RpcUpdateVelocity(Vector3 v, Vector3 p) { // sono sul client
+
+        if (hasAuthority) {
+            // Hey, this is my own object. I "should" already have the most accurate
+            // position/velocity (possibly more "Accurate") than the server
+            // Depending on the game, I MIGHT want to change to patch this info
+            // from the server, even though that might look a little wonky to the user.
+
+            // Let's assume for now that we're just going to ignore the message from the server.
+            return;
+        }
+
+        // I am a non-authoratative client, so I definitely need to listen to the server.
+        // If we know what our current latency is, we could do something like this: transform.position = p + (v * (ourLatency))
+
+        //transform.position = p;
+        velocity = v;
+        bestGuessPosition = p + (velocity * (ourLatency));
+
+        // Now position of player one is as close as possible on all player's screens
+        // IN FACT, we don't want to directly update transform.position, because then players will keep teleporting/blinking as the updates come in. It looks dumb.
+    }
+}
